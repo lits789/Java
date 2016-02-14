49d647c5caf0a0e4e09ad283c227155551dbed1f
dibs <lits789@gmail.com>
2016-02-14 01:48:50 -0800

    Add src

---
+++ src/day1/examples/MyFirstClass.java
@@ -0,0 +1,16 @@
+package day1.examples;
+
+public class MyFirstClass {
+
+	/*
+	 * This is my first
+	 * Java Program
+	 * I am new to Java
+	 */
+	public static void main(String[] args) {
+	 //My first statement
+		System.out.println("Hello World");
+
+	}
+
+}
